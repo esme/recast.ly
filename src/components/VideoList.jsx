77a02b03da190d7a7cab8f969950d1d835fc5a9e
esme <esmelingyr@gmail.com>
2019-06-10 20:16:49 -0700

    finished half of basics

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,16 @@
-var VideoList = () => (
+
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map((el, i) => 
+      <VideoListEntry key={i} video={el}/>  
+    )}
+    {/* <VideoListEntry video={props.videos[0]}/>
+    <VideoListEntry video={props.videos[1]}/>
+    <VideoListEntry video={props.videos[2]}/>
+    <VideoListEntry video={props.videos[3]}/>
+    <VideoListEntry video={props.videos[4]}/> */}
   </div>
 );
 
